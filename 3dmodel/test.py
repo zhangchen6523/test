bef6906ec10636266319620149bff80c40c4d097
Joe <zhangchen6523@163.com>
2019-09-09 14:27:13 +0800

    修改python执行路径

--- 3dmodel/test.py
+++ 3dmodel/test.py
@@ -7,14 +7,18 @@
 # l = Label(root, text="label", bg="pink", font=("Arial", 12), width=8, height=3)
 # l.pack(side=LEFT)
 # root.mainloop()
-# import numpy as np
-# a = np.array([2,3,4],dtype=np.int)
-# # print(a)
+# import sys
+# print(sys.path)
+import numpy as np
+a = np.array([[2,3,4],[5,6,7]],dtype=np.int)
+print(a.sum(axis=1))
+print(a.shape)
 #
 # A = np.random.randint(0,10,size=(3,4,5,6))
 #
 #
 # testa = np.arange(4)
+# print(help(np.arange))
 # print(testa)
 #
 # b = testa.copy()
@@ -22,7 +26,7 @@
 # print(testa)
 # print(b)
 
-def test(text):
-    print(text)
-
-test(aaa="aaa")
+# def test(text):
+#     print(text)
+#
+# test(aaa="aaa")
